8076836f00fd053438067c022f58a27608ab5fbb
Reynaldi Lusikooy <andre.reynaldi@ai.astra.co.id>
2024-04-15 11:04:49 +0700

    feat: jsdocs and profiling fix

--- src/util/setFile.js
+++ src/util/setFile.js
@@ -2,21 +2,38 @@
 
 const fs = require('fs');
 
+/**
+ * A class for handling file operations.
+ */
 class FileHandler {
 
+    /**
+     * Reads a file synchronously.
+     * 
+     * @param {string} fileInputName - The name of the input file.
+     * @param {string} encoding - The encoding of the file.
+     * @returns {string} The content of the file as a string.
+     */
     readFile(fileInputName, encoding) {
         return fs.readFileSync(fileInputName, encoding).toString();
     }
 
+    /**
+     * Writes JSON data to a file.
+     * 
+     * @param {string} json - The JSON data to write to the file.
+     * @param {string} fileOutputName - The name of the output file.
+     */
     writeFile(json, fileOutputName) {
-        const start = process.hrtime.bigint();
+        let getDuration = null;
+        const start = performance.now();
         fs.writeFile(fileOutputName, json, function (err) {
-            const end = process.hrtime.bigint();
-            const duration = (end - start) / BigInt(1e6);
+            const end = performance.now();
+            getDuration = `${(end - start).toFixed(2)} ms`;;
             if (err) {
                 throw err;
             } else {
-                console.log('\x1b[32m', '✔️ ', `Conversion successful: ${fileOutputName} (${duration} ms)`, '\x1b[0m');
+                console.log('\x1b[32m', '✔️ ', `Conversion successful on: ${fileOutputName} (\x1b[36m${getDuration}\x1b[0m)`, '\x1b[0m');
             }
         });
     }
