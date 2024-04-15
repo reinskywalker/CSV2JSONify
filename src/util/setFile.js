fa6d08c0637bcd2a99ba6b6e69d5bc734c5745b6
Reynaldi Lusikooy <andre.reynaldi@ai.astra.co.id>
2024-04-15 10:54:45 +0700

    feat: runtime profiling

--- src/util/setFile.js
+++ src/util/setFile.js
@@ -1,22 +1,26 @@
 'use strict';
 
-let fs = require('fs');
+const fs = require('fs');
 
-class SetFile {
+class FileHandler {
 
     readFile(fileInputName, encoding) {
         return fs.readFileSync(fileInputName, encoding).toString();
     }
 
     writeFile(json, fileOutputName) {
+        const start = process.hrtime.bigint();
         fs.writeFile(fileOutputName, json, function (err) {
+            const end = process.hrtime.bigint();
+            const duration = (end - start) / BigInt(1e6);
             if (err) {
                 throw err;
             } else {
-                console.log('File saved: ' + fileOutputName);
+                console.log('\x1b[32m', '✔️ ', `Conversion successful: ${fileOutputName} (${duration} ms)`, '\x1b[0m');
             }
         });
     }
 
 }
-module.exports = new SetFile();
+
+module.exports = new FileHandler();
